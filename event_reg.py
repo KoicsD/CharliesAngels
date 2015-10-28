cbdfbd9d2bfe047610077b8fa6c5fef4864656aa
Zsolt Bodi <bodizsolt1992@gmail.com>
2015-10-28 11:50:02 +0100

    Date and time implemened (but not tested) in event_reg.

--- event_reg.py
+++ event_reg.py
@@ -1,5 +1,5 @@
 __author__ = 'KoicsD'
-
+from datetime import datetime, time, timedelta
 
 # Date of event and start and end time
         # Registration should occur before the event at least 10 days
@@ -18,7 +18,7 @@ __author__ = 'KoicsD'
         # How many bed will be available? (Only positive integers allowed)
         # Planned donor number (Only positive integers allowed)
         # max_donor_number =
-        # ((event_duration_in_minutes - preparation_time) / donation_time) * number_of_bedsû
+        # ((event_duration_in_minutes - preparation_time) / donation_time) * number_of_beds
 #
         # preparation_time = 30
         # donation_time = 30
@@ -36,8 +36,73 @@ __author__ = 'KoicsD'
 # Calculate duration in minutes based on start and endtime
 # You can use datetime.strptime and  timedelta.total_seconds functions
 # max_donor_number =
-# ((event_duration_in_minutes - preparation_time) / donation_time) * number_of_bedsû
+# ((event_duration_in_minutes - preparation_time) / donation_time) * number_of_beds
 #
 # preparation_time = 30
 # donation_time = 30
 # At the end of the event registration ask the user about how many successfull donation was on the event and store it as an integer!
+
+
+class Donation:
+    preparation_time = 30
+
+    def __init__(self):
+        self.date = datetime(0).date()
+        self.start_time = time()
+        self.end_time = time()
+        self.duration_time = 0
+        self.valid = False
+
+    def input_date(self):
+        sdate = ""
+        while True:
+            sdate = input("Please enter date of event (YYYY.MM.DD): ")
+            try:
+                pdate = datetime.strptime(sdate, "%Y.%m.%d").date()
+                time_until_event = pdate - datetime.now().date()
+                msg = "OK"
+                if time_until_event.days < 10:
+                    msg = "Date must be at least 10 days before the event!"
+                elif not 1 <= pdate.isoweekday() <= 5:
+                    msg = "Date must be on weekday!"
+                if msg == "OK":
+                    break
+                else:
+                    print(msg)
+            except ValueError:
+                print("Wrong date format!")
+        self.date = pdate
+
+    def input_start_time(self):
+        starttime = ""
+        while True:
+            starttime = input("Please enter the start time of event (HH:MM): ")
+            try:
+                pstarttime = datetime.strptime(starttime, "%H:%M").time()
+                break
+            except ValueError:
+                print("Wrong format for time!")
+        self.start_time = pstarttime
+
+    def input_end_time(self):
+        endtime = ""
+        while True:
+            endtime = input("Please enter the end time of event (HH:MM): ")
+            try:
+                pendtime = datetime.strptime(endtime, "%H:%M")
+                pstarttime = datetime.combine(self.date, self.start_time)
+                duration = (pendtime - pstarttime).total_seconds() // 60
+                msg = "OK"
+                if duration < Donation.preparation_time:
+                    msg = "Preparation Time exceeds Duration or Negative Duration!"
+                if msg == "OK":
+                    break
+                else:
+                    print(msg)
+            except ValueError:
+                print("Wrong format for time!")
+        self.end_time = pendtime
+        self.duration = duration
+
+
+
