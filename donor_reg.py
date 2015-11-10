a779b85725b6075bf0562dab54d9a1cbbb04b758
Viczmándi Gergely <viczmandi@gmail.com>
2015-11-10 23:22:01 +0100

    kommenteket kitöröltem, feleslegesnek és zavarónak találtam

--- donor_reg.py
+++ donor_reg.py
@@ -2,13 +2,6 @@ from datetime import datetime, date
 import random
 
 
-# Task:
-#     warn_user:# weight > 50
-#                 Generate random number: Hemogblobin level between 80-200, write out is the donor suitable or not (value is greather than 110)?
-#                 last donation was more than 3 months ago
-#                 age > 18 years
-#                 How old is the donor in years based on date of birth?
-#                 ID is not expired.
 def warn_user():
     inp = ""
     l_inp = ""
@@ -59,11 +52,6 @@ def warn_user():
 def random_hemoglobin():
     return random.randint(80, 200)
 
-# -------------------------------------------------------------------------------
-#
-#     Name,
-#                 Parse name, store it in a separated object
-#
 
 def validate_name(name_string: str):
     splitted_name = name_string.split(" ")
@@ -83,12 +71,6 @@ def input_name():
     return data_name
 
 
-# -------------------------------------------------------------------------------
-#
-#     Weight,
-#                 weight > 50
-#
-
 def input_weight():
     data_weight = ""
     while not data_weight:
@@ -98,16 +80,6 @@ def input_weight():
             data_weight = ""
 
     return int(data_weight)
-
-
-# -------------------------------------------------------------------------------
-#
-#
-#     Gender,
-#
-#     Date of Birth,
-#                 age > 18 years
-#                 How old is the donor in years based on date of birth?
 
 
 def get_birth_date():
@@ -125,11 +97,6 @@ def get_birth_date():
 
 def calculate_age_in_year(birth_date: date):
     return (datetime.now().date() - birth_date).days // 365
-
-#
-#     Last donation date,
-#                 last donation was more than 3 months ago
-#                 never is also possible
 
 
 def get_last_donation_time():
@@ -152,17 +119,6 @@ def last_donation_time_is_valid(date_of_donation):
         return date_of_donation
     else:
         return (datetime.now().date() - date_of_donation).days > 90
-#
-#     Unique identifier  &     Expiration of ID
-#                 6digit + 2letter (123456AB) is identity card
-#                 6letter + 2digit (ASDFGH12) is passport
-#                 ID is not expired.
-# ID_expired=""
-# def get_ID_expired():
-#     ID_expired=input ("When will ID be expired?(YYYY/MM/DD")
-#
-# def check_ID_expiration(date):
-#     return ID_expired > today
 
 
 def input_id_expiration():
@@ -219,9 +175,6 @@ def validate_identifier():
     return identifier
 
 
-# Blood type
-#
-
 def input_blood_type():
     valid_blood_type = False
     data_blood_type = ""
@@ -236,12 +189,6 @@ def input_blood_type():
 
     return data_blood_type
 
-
-# -------------------------------------------------------------------------------
-#     email address
-#                 Email address validation (contains @-ot and ending with .hu/.com)
-#     Mobil number
-#                 Mobil number validation (starts with +36/06 + 2 digit(provider identifier - 20/30/70) ending with 7 digits)
 
 def check_mobil_number(mobile_number):
     if mobile_number[0:2] != '06' and mobile_number[0:3] != "+36":
@@ -272,35 +219,6 @@ def get_mobile_number():
         elif not check_mobil_number(mobile_number):
             mobile_number = ""
     return mobile_number
-
-
-#     __repr__
-#                 Write out data in a table form pl.:
-#                 Attila, Molnar
-#                 90kg [using of str function]
-#                 1989.05.06 - 26 years old
-#                 asd@test.hu,
-#                 Generate random number: Hemogblobin level between 80-200, write out is the donor suitable or not (value is greather than 110)?
-#
-# Functions:
-# Parse name, store it in a separated object                    used
-# Suitable for donation:                                        used
-# weight > 50
-# last donation was more than 3 months ago
-# age > 18 years
-# How old is the donor in years based on date of birth?
-# ID is not expired.                                            used
-# Define type of personal document based on its identifier:
-# 6digit + 2letter (123456AB) is identity card                  used
-# 6letter + 2digit (ASDFGH12) is passport                       used
-# Email address validation (contains @-ot and ending with .hu/.com)         used
-# Mobil number validation (starts with +36/06 + 2 digit(provider identifier - 20/30/70) ending with 7 digits)   used
-# Write out data in a table form pl.:
-# Attila, Molnar
-# 90kg [using of str function]
-# 1989.05.06 - 26 years old
-# asd@test.hu,
-# Generate random number: Hemogblobin level between 80-200, write out is the donor suitable or not (value is greather than 110)?
 
 
 def gender_is_valid(string):
