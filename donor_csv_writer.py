5ca0ecfa00415ae4bc4dc5c6470a4e7a3cc5b4d3
Viczmándi Gergely <viczmandi@gmail.com>
2015-11-11 10:44:30 +0100

    hemoglobin is tárolásra kerül

--- donor_csv_writer.py
+++ donor_csv_writer.py
@@ -2,11 +2,11 @@ __author__ = 'viczmandi'
 import csv
 
 def store_donor(name, age, gender, birth_date, id, id_expiration,
-                weight, blood_type, last_donation,
+                weight, blood_type, hemoglobin, last_donation,
                 mobile, email):
 
     with open("DATA\donors.csv", "a", newline="") as csv_file:
         csv_file_writer = csv.writer(csv_file)
         csv_file_writer.writerow([name, age, gender, birth_date, id, id_expiration,
-                weight, blood_type, last_donation,
+                weight, blood_type, hemoglobin, last_donation,
                 mobile, email])
