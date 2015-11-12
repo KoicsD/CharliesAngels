cfdc8c98561162434906dc11d8df8e567fc8af19
Daniel Koics <koics.dani@gmail.com>
2015-11-12 15:02:15 +0100

    data handler adds and deletes events

--- data_handler.py
+++ data_handler.py
@@ -1,28 +1,63 @@
 __author__ = 'ozsvarmarton'
 import event_reg
-import donor_reg
-from random import randint
+import csv
 
 
-our_events = {}
-our_donors = {}
+our_events = []
 
 
 def read():
-    with open("DATA/event.csv", "r") as file_obj:
-        data = file_obj.readlines()
-        for item in data:
-            pass
+    with open("DATA/event.csv", "r", newline='') as file_obj:
+        new_events = []
+        reader = csv.reader(file_obj)
+        # assert header.index("id") == 0
+        next(reader)
+        for row in reader:
+            data = row[1:]
+            new_events.append(event_reg.Donation.from_lists(data))
+    global our_events
+    our_events.clear()
+    our_events += new_events
+
 
 def write():
-    with open("DATA/event.csv", "w") as file_obj:
-        for key, event in our_events.items():
-            file_obj.write(key + "," + event.to_csv() + "\n")
+    with open("DATA/event.csv", "w", newline='') as file_obj:
+        global our_events
+        writer = csv.writer(file_obj)
+        writer.writerow(["id"] + event_reg.Donation.header)
+        for ind in range(len(our_events)):
+            writer.writerow([str(ind + 1)] + our_events[ind].to_lists())
+
 
 def add_event():
-    new_event = event_reg.Donation()
-    while True:
-        new_key = hex(randint(0, 16 ** 4 - 1))
-        if new_key not in our_events.keys():
-            break
-    our_events[new_key] = new_event
+    new_event = event_reg.Donation.from_user()
+    our_events.append(new_event)
+    write()
+
+def remove_event(index: int):
+    global our_events
+    our_events.pop(index - 1)
+    write()
+
+
+def demo():
+    print("Adding 2 events:")
+    print("First...")
+    add_event()
+    print("Second...")
+    add_event()
+    print("Done.")
+    print("Removing 2nd element...")
+    remove_event(2)
+    print("Done.")
+    print("Adding another element...")
+    add_event()
+    print("Done.")
+    print("Demo terminates.")
+
+
+read()
+
+
+if __name__ == '__main__':
+    demo()
