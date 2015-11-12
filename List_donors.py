ae499cb4d7c0af5881b6ea03d80ec518ad6f683f
Zsolt Bodi <bodizsolt1992@gmail.com>
2015-11-12 12:19:43 +0100

    list_donors now do not list in sorted, but can ignore the first  index line

--- List_donors.py
+++ List_donors.py
@@ -4,26 +4,40 @@ import csv
 def listing():
     with open('DATA\donors.csv', newline='') as file:
         reader = csv.reader(file)
-        reader = sorted(reader)
-        index = 1
-        for rows in reader:
-            print("-" * 22)
-            next_page = input("Press enter to next page or enter 'exit' to return to the main menu.")
-            print("-" * 22)
-            print(str(index) + ".")
+        list = reader
+        index = 0
+        next_page = ""
+        for rows in list:
+            if index > 0:
+                print("-" * 68)
+                next_page = input("Press enter to next page or enter 'exit' to return to the main menu.")
+                print("-" * 68)
+                print(str(index) + ". page")
             index += 1
             result = []
             if next_page.lower() == "exit":
                 return
             for row in rows:
-                for item in row:
-                    result.append(row)
-                    break
-            print(result[0])
-            print(result[6] + "kg")
-            print(result[3] + " - " + result[1] + " years old")
-            print(result[10])
+                if row not in exceptions:
+                    for item in row:
+                        result.append(row)
+                        break
+            try:
+                print_donors_data(index, result)
+            except IndexError:
+                continue
+        print("-" * 39)
+        exit = input("Press enter to return to the main menu.")
+        print("-" * 39)
 
+
+def print_donors_data(index,result):
+        print(result[0])
+        print(result[6] + "kg")
+        print(result[3] + " - " + result[1] + " years old")
+        print(result[10])
+
+exceptions = ["name","weight","gender","date_of_birth","last_donation","last_month_sickness","unique_identifier","expiration_of_id","blood_type","hemoglobin","email","mobil"]
 def main():
     listing()
 
