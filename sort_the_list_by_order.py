4d35f6db774423760f00fb415f1ec52b5199d3f7
Zsolt Bodi <bodizsolt1992@gmail.com>
2015-11-25 11:01:21 +0100

    search results printed in corrected form

--- sort_the_list_by_order.py
+++ sort_the_list_by_order.py
@@ -1,4 +1,4 @@
-
+from search_in_files import calculate_age_in_year
 import csv
 
 def sorting(lista):
@@ -7,7 +7,7 @@ def sorting(lista):
 
         lst.append(str(i).lower())
 
-    if len(lst)==1:
+    if len(lst) == 1:
         good.append(min(lst))
 
         return
@@ -25,8 +25,16 @@ def sorting_row(index,good_index):
     with open('DATA/donors.csv', newline='') as donor_file:
         reader = csv.reader(donor_file)
         for row in reader:
-            if row[index].lower()==good[good_index]:
-                print (row)
+            if row[index].lower() == good[good_index]:
+                if "name" in row:
+                    continue
+                print("-"*35)
+                print(str(index) + ".")
+                print("\t" + row[0])
+                print("\t" + row[1] + " kg")
+                print("\t" + row[3] + " - " + str(calculate_age_in_year(row[3])) + " years old")
+                print("\t" + row[10])
+                print("-" * 35)
         good_index += 1
         if good_index<len(good):
             sorting_row(index,good_index)
