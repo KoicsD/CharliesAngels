9366b440f03e893985f3ce62d36c1301ed2c020a
unknown <szekely.zoltan0321@gmail.com>
2015-11-26 10:51:05 +0100

    sort_donor_by_order is okay

--- sort_by_order.py
+++ sort_by_order.py
@@ -2,20 +2,38 @@ __author__ = 'Zoltan'
 from search_in_files import calculate_age_in_year
 import csv
 
-index = int(input('What will it get ordered by?'))
-row_index= 1
-with open('DATA/donors.csv',newline='') as donor_file:
+
+def sorting_donor_by_order():
+    with open('DATA/donors.csv', newline='') as donor_file:
+        index = input('What will donors get ordered by?'+'\n'
+                          'name:0,'+'\n'
+                          'weight:1'+'\n'
+                          'gender:2'+'\n'
+                          'date_of_birth:3'+'\n'
+                          'last_donation:4'+'\n'
+                          'last_month_sickness:5'+'\n'
+                          'unique_identifier:6'+'\n'
+                          'expiration_of_id:7'+'\n'
+                          'blood_type:8'+'\n'
+                          'hemoglobin:9'+'\n'
+                          'email:10'+'\n'
+                          'mobil:11')
+        list_of_index = ['0','1','2','3','4','5','6','7','8','9','10','11']
+        if index not in list_of_index:
+            index=0
+        else:
+            index=int(index)
+        row_index = 1
         reader = csv.reader(donor_file)
         next(reader)
-        lst=[]
+        lst = []
         if index == 1 or index == 9:
             lst = sorted(reader, key=lambda donor: int(donor[index]))
         else:
-            lst = ( sorted(reader, key=lambda donor: donor[index]))
+            lst = (sorted(reader, key=lambda donor: donor[index]))
 
         for row in lst:
-
-            print("-"*35)
+            print("-" * 35)
             print(str(row_index) + ".")
             row_index += 1
             print("\t" + row[0])
@@ -23,3 +41,7 @@ with open('DATA/donors.csv',newline='') as donor_file:
             print("\t" + row[3] + " - " + str(calculate_age_in_year(row[3])) + " years old")
             print("\t" + row[10])
             print("-" * 35)
+
+
+
+sorting_donor_by_order()
