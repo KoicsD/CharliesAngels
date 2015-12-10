bee48e2b93ca3bcc2bc4df85f5c3326990ce53d5
Zsolt Bodi <bodizsolt1992@gmail.com>
2015-12-10 10:32:52 +0100

    donor_reg main now returns a list instead running the csv writer directly modified the other modules too where needed

--- donor_csv_writer.py
+++ donor_csv_writer.py
@@ -1,11 +1,10 @@
 __author__ = 'viczmandi'
 import csv
 
-def store_donor(name,weight,gender,birth_date,
-                                 last_donation,was_sick_in_last_month,
-                                 id,id_expiration,
-                                 blood_type,hemoglobin,email,mobile):
 
+def store_donor(data):
+    name,weight,gender,birth_date,last_donation,was_sick_in_last_month,\
+    id,id_expiration,blood_type,hemoglobin,email,mobile = data
     with open("DATA/donors.csv", "a", newline="") as csv_file:
         csv_file_writer = csv.writer(csv_file)
         csv_file_writer.writerow([name,weight,gender,birth_date,
