e786df0d46875e123f8560501b0c5ad01629566d
Daniel Koics <koics.dani@gmail.com>
2015-12-10 11:09:36 +0100

    function sql_handle.initialize created, but cannot be used or tested as main.read_config not ready

--- sql_handler.py
+++ sql_handler.py
@@ -1,6 +1,13 @@
-import mysql.connector
+import mysql.connector as sql
 import donor_reg, event_reg
 
 
-def initialize():
-    pass
+connection_obj = None
+cursor_obj = None
+
+
+def initialize(connection_data: dict):
+    global connection_obj, cursor_obj
+    connection_obj = sql.connect(**connection_data)
+    cursor_obj = connection_obj.cursor()
+    print("connection established!!!")
